ee2e1eb9461795cd288a94918676cef6b0ce7554
전대현 <detail54@nate.com>
2021-08-14 11:52:46 +0900

    init

---
+++ src/components/todo/TodoTemplate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style/TodoTemplateStyle.scss';
+
+const TodoTemplate = ({ children }) => {
+  return (
+    <>
+		<div className="TodoTemplate">
+			<div className="app-title">일정 관리</div>
+			<div className="content">{children}</div>
+		</div>
+    </>
+  )
+}
+
+export default TodoTemplate
+ 
