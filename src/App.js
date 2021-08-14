21b31d4c0e2ba2a55f6e8ed240c41f3a92b2ce8e
전대현 <detail54@nate.com>
2021-08-14 15:06:37 +0900

    Feat: Add TodoList Insert

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import TodoInsert from "./components/todo/TodoInsert";
 import Todolist from "./components/todo/Todolist";
 import TodoTemplate from "./components/todo/TodoTemplate";
@@ -22,9 +22,25 @@ const App = () => {
     },
   ]);
 
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert}/>
       <Todolist todos={todos}/>
     </TodoTemplate>
   )
