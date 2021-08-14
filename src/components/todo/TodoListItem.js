33c20a66118c7c77f2ae3e157c85dafa1c3bea7e
전대현 <detail54@nate.com>
2021-08-14 17:42:40 +0900

    Feat: Update TodoList Use react-virtualized

--- src/components/todo/TodoListItem.js
+++ src/components/todo/TodoListItem.js
@@ -3,23 +3,28 @@ import { MdCheckBoxOutlineBlank, MdRemoveCircleOutline, MdCheckBox } from 'react
 import './style/TodoListItemStyle.scss';
 import cn from 'classnames';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
 
   const { id, text, checked } = todo;
 
   return (
     <>
-      <div className='TodoListItem'>
-        <div className={ cn('checkbox', { checked }) } onClick={()=>{onToggle(id)}}>
-          {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank />}  
-          <div className='text'>{text}</div>
-        </div>  
-        <div className='remove' onClick={() => onRemove(id)}>
-          <MdRemoveCircleOutline />
-        </div>
-      </div>         
+      <div className='TodoListItem-virtualized' style={style}>
+        <div className='TodoListItem'>
+          <div className={ cn('checkbox', { checked }) } onClick={()=>{onToggle(id)}}>
+            {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank />}  
+            <div className='text'>{text}</div>
+          </div>  
+          <div className='remove' onClick={() => onRemove(id)}>
+            <MdRemoveCircleOutline />
+          </div>
+        </div>     
+      </div>    
     </>
   )
 }
 
-export default React.memo(TodoListItem)
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo
+)
