7e2935bd50345ac68dc4739f8a4c6db8a2c74ae2
전대현 <detail54@nate.com>
2021-08-18 22:25:42 +0900

    Feat: Add useImmer Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import TodoIndex from './components/todo/TodoIndex';
+import UseImmerIndex from "./components/useImmer/UseImmerIndex";
 
 const App = () => {
 
   return (
-    <TodoIndex />
+    <>
+      <TodoIndex />
+      <UseImmerIndex />
+    </>
   )
 };
 
