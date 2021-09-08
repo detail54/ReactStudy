041f8e0a896d81effcf66525c92723d0a78e43f0
전대현 <detail54@nate.com>
2021-09-08 19:48:10 +0900

    Feat: history block

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, Route } from "react-router-dom";
+import HistoryTutoIndex from "./components/historytutorial/HistoryTutoIndex";
 import RouterTutorialIndex from "./components/routertutorial/RouterTutorialIndex";
 import TodoIndex from './components/todo/TodoIndex';
 
@@ -13,10 +14,14 @@ const App = () => {
         <li>
           <Link to="/todo">일정관리</Link>
         </li>
+        <li>
+          <Link to="/history">히스토리</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={RouterTutorialIndex} exact={true}/>
       <Route path="/todo" component={TodoIndex} />
+      <Route path="/history" component={HistoryTutoIndex} />
     </>
   )
 };
