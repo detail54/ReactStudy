d0e7976dc864b905cbe5de1e9de9ba5456a32c2d
전대현 <detail54@nate.com>
2021-09-12 17:57:52 +0900

    feat: Add mouse click event in contexttuto

--- src/components/contexttutorial/contextIndex.js
+++ src/components/contexttutorial/contextIndex.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ColorBox from './ColorBox'
 import { ColorProvider } from '../../store/color';
+import SelectColors from './SelectColors';
 
 
 const contextIndex = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
