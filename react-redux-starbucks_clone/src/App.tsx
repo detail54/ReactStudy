9c609448123bc49d400889667762cdc4a960455f
DaeHyun Jun <detail54@nate.com>
2021-10-26 14:13:18 +0900

    feat: Add react-router-dom

--- react-redux-starbucks_clone/src/App.tsx
+++ react-redux-starbucks_clone/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
 import Menu from './components/menu/Menu'
+import Main from './containers/Main'
 
 const App: React.FC = () => {
   return (
-    <>
+    <BrowserRouter>
       <Menu />
-    </>
+      <Main />
+    </BrowserRouter>
   )
 }
 
